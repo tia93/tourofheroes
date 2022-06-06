bc3af7e2be243ba0ce607ba9e4f8f4a1344674e0
tia93 <103486517+tia93@users.noreply.github.com>
2022-06-06 12:12:27 +0200

    1 parte

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements OnInit{
+
+  title: string = 'lista studenti migliori'; // riportato sul html
+  // students: string[] = ['pippo', 'pluto', 'paperina', 'mimmie', 'paperone', 'topolino', 'gaston']
+ students2: any[] = [ /// riportaro su html
+   {
+    name: 'pippo',
+    grade: 10
+   },
+   {
+     name: 'pluto',
+     grade: 6
+   },
+   {
+    name: 'paerino',
+    grade: 7
+   },
+   {
+     name: 'paperone',
+     grade: 6
+   }
+  ]
+  constructor(){
+
+  }
+
+  ngOnInit(){
+  // const container = document.getElementById('app-container');
+  // const h1 = document.createElement('h1');
+  // const node = document.createTextNode(this.title);
+
+  // h1.appendChild(node),
+  // container?.appendChild(h1)
+
+  // const ul = document.createElement('ul')
+  // for (const item of this.students) {
+  //   const li = document.createElement('li');
+  //   const node = document.createTextNode(item);
+  //   li.appendChild(node);
+  //   ul.appendChild(li);
+  // }
+
+  // container?.appendChild(ul);
+  }
+
+}
